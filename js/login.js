9bcb5c90d4f316eb2df6d89caf86413c55d0c4c8
SamheinDM <SamheinDM@yandex.ru>
2021-01-22 19:21:41 +0300

    feat: add login screen

--- js/login.js
+++ js/login.js
@@ -3,6 +3,8 @@
 import create from './utils/create.js';
 import NetAPI from './network_api.js';
 import { set } from './utils/storage.js';
+import { even } from 'prelude-ls';
+import ChatList from './chat_list.js';
 
 export default class Login {
   constructor() {
@@ -32,8 +34,48 @@ export default class Login {
     }
   }
 
+  clearLoginScreen() {
+    const childList = Array.from(this.wrapper.childNodes);
+    for (let i = 0; i < childList.length; i += 1) {
+      this.wrapper.removeChild(childList[i]);
+    }
+  }
+
+  initRegistration(event) {
+    event.preventDefault();
+    this.clearLoginScreen();
+
+    const form = create('form', 'login_form', this.wrapper, ['id', 'register_form']);
+    create('label', 'login_label', form, ['for', 'login_reg_input'], ['textContent', 'Введите логин: ']);
+    this.loginInput = create('input', 'login_input', form,
+      ['id', 'login_reg_input'],
+      ['required', true]);
+    create('label', 'login_label', form, ['for', 'pass_reg_input'], ['textContent', 'Введите пароль: ']);
+    this.passwordInput = create('input', 'login_input', form,
+      ['id', 'pass_reg_input'],
+      ['required', true],
+      ['type', 'password'],
+      ['autocomplete', 'new-password']);
+    create('label', 'login_label', form, ['for', 'pass_reg_rep_input'], ['textContent', 'Повторите пароль: ']);
+    this.passwordInput = create('input', 'login_input', form,
+      ['id', 'pass_reg_rep_input'],
+      ['required', true],
+      ['type', 'password'],
+      ['autocomplete', 'new-password']);
+    const btnWrapper = create('div', 'login_btn_wrapper', form);
+    const regBtn = create('button', 'login_btn', btnWrapper, ['type', 'submit'], ['textContent', 'Зарегестрироваться']);
+    const cancelBtn = create('button', 'login_btn', btnWrapper, ['textContent', 'Отмена']);
+
+    cancelBtn.addEventListener('click', () => this.init());
+  }
+
   init() {
-    this.wrapper = create('div', 'main_wrapper login_screen', this.body);
+    if (this.wrapper) {
+      this.clearLoginScreen();
+    } else {
+      this.wrapper = create('div', 'main_wrapper login_screen', this.body);
+    }
+
     const form = create('form', 'login_form', this.wrapper, ['id', 'login_form']);
     create('label', 'login_label', form, ['for', 'login_input'], ['textContent', 'Login: ']);
     this.loginInput = create('input', 'login_input', form,
@@ -56,6 +98,6 @@ export default class Login {
     this.passwordInput.onfocus = () => this.removeErrMsg();
 
     loginBtn.addEventListener('click', (e) => e.preventDefault);
-    registerBtn.addEventListener('click', (e) => e.preventDefault());
+    registerBtn.addEventListener('click', (e) => this.initRegistration(e));
   }
 }
